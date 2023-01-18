131d180f27205f09341829daf27e3b83fdb13a60
Dima <kutsenko1902@gmail.com>
2023-01-18 23:02:33 +0200

    Practik task 2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,51 @@
-"use strict";
+/* Задание на урок:
 
-let num = 50;
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-do {
-    console.log(num);
-    num++; 
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i =0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a!= '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+       console.log('done'); 
+    } else {
+        console.log('error');
+        i--;
+    }
+
 }
-while (num < 55);
 
-for (let i = 1; i < 8; i++) {
-    console.log(i);
-    num++;
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else (personalMovieDB.count >= 10 && < 30) {
+console.log("Вы классический зритель");
+} else if (personalMovieDB >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
 }
+
+console.log(personalMovieDB);
